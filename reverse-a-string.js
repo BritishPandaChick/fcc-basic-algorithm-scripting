1619335f9e6dda2de9450f3464c468a457902a81
BritishPandaChick <sbartley1080@gmail.com>
2017-11-20 18:25:09 -0500

    Add reverse a string

---
+++ reverse-a-string.js
@@ -0,0 +1,9 @@
+function reverseString(str){
+  str = str.split("");  //Split the string into an array of characters for each space of ''
+  str = str.reverse();  //Use array function reverse to reverse the vaules of index from first to last and vice versa
+  str = str.join([separator = '']);  //reset to a string using array's joint function and adding the separator ''
+  return str;
+}
+
+reverseString("hello");
+reverseString("Greetings from Earth");
